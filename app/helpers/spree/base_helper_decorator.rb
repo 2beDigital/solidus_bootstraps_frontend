3cbb53650f2160c38795a6b1b19125351dc601ab
David Stocker <stockerd@gmail.com>
2014-07-28 11:58:11 -0700

    fixing bug with flash msg ignore type evaluating on strings instead of symbols

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -18,7 +18,7 @@ Spree::BaseHelper.module_eval do
     opts[:ignore_types] = [:order_completed].concat(Array(opts[:ignore_types]) || [])
 
     flash.each do |msg_type, text|
-      unless opts[:ignore_types].include?(msg_type)
+      unless opts[:ignore_types].include?(msg_type.to_sym)
         concat(content_tag :div, text, class: "alert alert-#{msg_type}")
       end
     end
