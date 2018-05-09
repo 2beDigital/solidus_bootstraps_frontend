32fdfeaf286da8ec41a97ae8dae6668dc91ddac1
Weston Ganger <westonganger@gmail.com>
2018-05-09 12:56:26 -0700

    Improve install generator

--- solidus_bootstrap/frontend/lib/generators/solidus_frontend_bootstrap/install/install_generator.rb
+++ solidus_bootstrap/frontend/lib/generators/solidus_frontend_bootstrap/install/install_generator.rb
@@ -5,11 +5,25 @@ module SolidusFrontendBootstrap
       
       source_root File.expand_path("../templates", __FILE__)
 
-      def copy_stylesheets
+      def add_stylesheets
         copy_file 'stylesheets/bootstrap_frontend.css.scss',
                   'app/assets/stylesheets/spree/frontend/bootstrap_frontend.css.scss'
-        copy_file 'stylesheets/all.css',
-                  'vendor/assets/stylesheets/spree/frontend/all.css'
+
+        string = "*= require spree/frontend/bootstrap_frontend"
+        
+        f = 'vendor/assets/stylesheets/spree/frontend/all.css'
+        gsub_regex = /^\*=(\ *)require spree\/frontend(?!\/)(.*)/
+        inject_regex = /^\*=(\ *)require_self/
+        
+        if File.readlines(f).grep(gsub_regex).any?
+          gsub_file f, gsub_regex do |match|
+            "#{match.sub('*=','*')}\n#{string}"
+          end
+        elsif File.readlines(f).grep(inject_regex).any?
+          inject_into_file f, "#{string}\n", before: inject_regex, verbose: true
+        else
+          copy_file 'stylesheets/all.css', 'vendor/assets/stylesheets/spree/frontend/all.css'
+        end
       end
     end
   end
